f6bdcd68c02d404ac9f39d9002069fc2c726e446
Queite <queitesc@gmail.com>
2022-12-16 19:36:05 -0300

    feat: :sparkles: implementa ingredientes a comprar

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,10 +16,19 @@ class InventoryControl:
     }
 
     def __init__(self):
-        pass
+        self.ingredients_to_buy = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
 
     def add_new_order(self, customer, order, day):
-        pass
+        for ing in self.INGREDIENTS[order]:
+            self.ingredients_to_buy[ing] += 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.ingredients_to_buy
